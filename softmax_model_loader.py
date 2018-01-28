93ef0e36eff045fa1f5587885fd4fee4932d28b1
vidit0210 <vidit0210@gmail.com>
2018-01-28 22:16:10 +0530

    First Commit

---
+++ softmax_model_loader.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('data', one_hot=True)
+sess = tf.InteractiveSession()
+new_saver = tf.train.import_meta_graph('data/saved_mnist_cnn.ckpt.meta')
+new_saver.restore(sess, 'data/saved_mnist_cnn.ckpt')
+tf.get_default_graph().as_graph_def()
+x = sess.graph.get_tensor_by_name("input:0")
+x=np.expand_dims(x,axis=1)
+x=tf.convert_to_tensor(x,dtype=tf.float32)
+y_conv = sess.graph.get_tensor_by_name("output:0")
+image_b = mnist.test.images[100]
+result = sess.run(y_conv, feed_dict={x:image_b})
+print(result)
+print(sess.run(tf.argmax(result, 1)))
+
+plt.imshow(image_b.reshape([28, 28]), cmap='Greys')
+plt.show()
+
+
+
+
+
